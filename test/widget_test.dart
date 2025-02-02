b3c1e946450faa95f21a20d03007d650779c73a1
CelaJest <johanrhys@gmail.com>
2025-02-02 18:36:46 -0500

    denp update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@ import 'package:invert/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp()); // Updated to MyApp
+    await tester.pumpWidget(const InVertApp()); // Updated to MyApp
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
