43f6eff850bd051d0c1aa17dc00e1033cd23a2ad
DomBie <drb0708@hotmail.com>
2025-02-07 15:44:55 -0500

    Database Field pulls

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:invert/home.dart';
@@ -19,6 +20,94 @@ class _HomepageState extends State<Homepage> {
     final String email = FirebaseAuth.instance.currentUser!.email.toString();
     final String name = FirebaseAuth.instance.currentUser!.displayName.toString();
     final String  invalidUser = 'No user is currently signed in';
+
+
+
+    Future<String> getFullName() async {
+      String fullname = '';
+       try {
+      DocumentSnapshot doc = await FirebaseFirestore.instance
+          .collection('users') // Replace with your collection name
+          .doc(email) // Replace with your document ID
+          .get();
+      if (doc.exists) {
+        fullname =doc.get('Full Name');
+        return fullname;
+      }
+      else {
+        return "No email exists!";
+      }
+
+      
+     
+    } catch (e) {
+      return "Error: $e";
+    }
+  }
+
+
+    Future<String> getUsernameFromCollection() async {
+      String fullname = '';
+       try {
+      DocumentSnapshot doc = await FirebaseFirestore.instance
+          .collection('users') // Replace with your collection name
+          .doc(email) // Replace with your document ID
+          .get();
+      if (doc.exists) {
+        fullname =doc.get('Username');
+        return fullname;
+      }
+      else {
+        return "No email exists!";
+      }
+
+      
+     
+    } catch (e) {
+      return "Error: $e";
+    }
+  }
+  
+    Future<String> getEmailFromCollection() async {
+      String fullname = '';
+       try {
+      DocumentSnapshot doc = await FirebaseFirestore.instance
+          .collection('users') // Replace with your collection name
+          .doc(email) // Replace with your document ID
+          .get();
+      if (doc.exists) {
+        fullname =doc.get('Email');
+        return fullname;
+      }
+      else {
+        return "No email exists!";
+      }
+
+      
+     
+    } catch (e) {
+      return "Error: $e";
+    }
+  }
+  Future<String> getTeamFromCollection() async {
+      String fullname = '';
+       try {
+      DocumentSnapshot doc = await FirebaseFirestore.instance
+          .collection('users') // Replace with your collection name
+          .doc(email) // Replace with your document ID
+          .get();
+      if (doc.exists) {
+        fullname =doc.get('Team');
+        return fullname;
+      }
+      else {
+        return "No email exists!";
+      }
+      } catch (e) {
+      return "Error: $e";
+    }
+  }
+
 
 
 
@@ -47,15 +136,32 @@ class _HomepageState extends State<Homepage> {
               ),
               Text('Username: $name\t'),
 
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()),
-                );
-                toastification.show(
+              FutureBuilder(future: getFullName(), builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator(); // Show loading spinner
+            } else if (snapshot.hasError) {
+              return Text("Error: ${snapshot.error}");
+            } else {
+              return Text(
+                "Full Name: ${snapshot.data}",
+                style: TextStyle(fontSize: 20),
+                  );
+                }
+              },
+            ),
+
+
+              const SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () {
+
+                  FirebaseAuth.instance.signOut();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                  toastification.show(
                   context: context,
                   type: ToastificationType.success,
                   style: ToastificationStyle.flat,
