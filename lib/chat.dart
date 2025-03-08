c449e8764a44d03d2088e33e14ecd853a228592e
Nicholas  Stancil <159091350+ndstancilFullsail@users.noreply.github.com>
2025-03-07 21:00:09 -0500

    Add

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,12 +1,8 @@
 import 'dart:async';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:invert/firebasefunctions.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 
 
 class ChatPage extends StatefulWidget {
