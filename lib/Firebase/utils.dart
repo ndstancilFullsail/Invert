598f6e24d21a9ddf035b27f6bf40f6c6820f09e9
DomBie <drb0708@hotmail.com>
2025-04-21 20:29:39 -0400

    Updated login

--- lib/Firebase/utils.dart
+++ lib/Firebase/utils.dart
@@ -2,5 +2,6 @@ import 'dart:ui';
 
 const maincolor = Color.fromARGB(255, 20, 107, 148);
 const maingray = Color.fromARGB(208,217, 217, 217);
+const mainwhite = Color.fromARGB(255, 255, 255, 255);
 
 
